4f8a16d3c7bc34bd5f104d261dc0fab64e701e54
Mestery <mestery@pm.me>
2021-04-27 14:29:11 +0200

    feat: form-data (#2)
    * feat: form-data
    
    Co-authored-by: Aakodal <48600424+Aakodal@users.noreply.github.com>

--- README.md
+++ README.md
@@ -11,3 +11,47 @@ $ npm install @typescord/famfor
 # or with Yarn
 $ yarn add @typescord/famfor
 ```
+
+## Usage
+
+Each FormData instance allows you to read its data from `stream.Readable` stream,
+just use `FormData#stream` for that.
+
+A `value` is considered as a file if it's a `ReadStream` or if it has a `filename`.
+When the `value` is a `ReadStream`, the `size` can be omitted, in this case, the `size`
+will be retrieved from the file system using `fs.statSync`.
+Else, if the `value` is a `Readable`, the `size` **should** be specified (if you want to have a correct length).
+
+If the `type` is present, it will always be put (even if the `filename` is not present).
+
+You can send queries via HTTP clients that support headers setting `stream.Readable` stream as body.
+
+Let's take a look at a minimal example with [Got](https://github.com/sindresorhus/got) :
+
+```ts
+import { FormData } from '@typescord/form-data'; // const { FormData } = require('@typescord/form-data');
+import got from 'got'; // const got = require('got');
+
+const fd = new FormData();
+// or `new FormData(false);` if you don't want the `Content-Length` header in `FormData#headers`
+
+fd.append('a_file', fs.createReadStream('./a_file.js'));
+fd.append('a_second_file', fs.createReadStream('./a_second_file.js'), { filename: 'a_better_name.js' });
+fd.append('another_field', 'My super field value');
+// when the value is a Readable, you **should** specify its size (if you want to have a correct length)
+fd.append('another_field', Readable.from('My super field value'), { size: 20 });
+fd.append('another_field', JSON.stringify({ content: 'another "another_field"' }), { type: 'application/json' });
+fd.append('yet_another_field', JSON.stringify({ content: "eééè'`e" }), { type: 'application/json; charset=utf-8' }); // for example
+
+// you can get the size (already included in the `headers`) simply with:
+console.log(fd.length);
+
+got
+	.post('https://example.com/', {
+		body: fd.stream, // set internal stream as request body
+		headers: fd.headers, // contains Content-Type and optionally Content-Length (see constructor's `contentLengthHeader` option)
+	})
+	.text()
+	.then(console.log)
+	.catch(console.error);
+```
---
+++ src/index.test.ts
@@ -0,0 +1,132 @@
+/* eslint-disable unicorn/no-useless-undefined */
+import { Readable } from 'stream';
+import { createReadStream } from 'fs';
+import { stat } from 'fs/promises';
+import { basename } from 'path';
+import { CRLF, DASHES, FormData } from '.';
+
+async function readableToString(readable: Readable): Promise<string> {
+	readable.setEncoding('utf8');
+	let stringified = '';
+	for await (const chunk of readable) {
+		stringified += chunk;
+	}
+	return stringified;
+}
+
+describe('FormData', () => {
+	it("should have some undefined Map's methods", () => {
+		const fd = new FormData();
+		expect(fd).toHaveProperty('forEach', undefined);
+		expect(fd).toHaveProperty('set', undefined);
+	});
+
+	it('should have a correct boundary', () => {
+		const boundary = new FormData().boundary;
+		expect(boundary.startsWith('----NodeJSFamforFormBoundary')).toStrictEqual(true);
+		expect(boundary).toHaveLength(56);
+	});
+
+	it('should have a correct footer', () => {
+		// @ts-expect-error footer is private
+		const { footer, boundary } = new FormData();
+		expect(footer).toStrictEqual(`${DASHES}${boundary}${DASHES}${CRLF}${CRLF}`);
+		expect(footer).toHaveLength(64);
+	});
+});
+
+describe('FormData#append', () => {
+	it('should return a correct body', async () => {
+		const fd = new FormData();
+		fd.append('"name"', 'value', { filename: '"name"' });
+		const body = await readableToString(fd.stream);
+		expect(body).toContain('name="\\"name\\""');
+		expect(body).toContain('filename="\\"name\\"');
+	});
+
+	it('should retrieve the length of the ReadStream', async () => {
+		const fd = new FormData();
+		const { size } = await stat(__filename);
+		fd.append('name', createReadStream(__filename), { filename: 'name' });
+		// @ts-expect-error footer is private
+		expect(fd.length).toStrictEqual(110 + size + fd.footer.length + fd.boundary.length);
+	});
+
+	it("shouldn't retrieve the length of the ReadStream when size is specified", async () => {
+		const fd = new FormData();
+		fd.append('name', createReadStream(__filename), { filename: 'name', size: 0 });
+		// @ts-expect-error footer is private
+		expect(fd.length).toStrictEqual(110 + fd.footer.length + fd.boundary.length);
+	});
+
+	it('should use ReadStream path as filename when the filename is not specified', async () => {
+		const fd = new FormData();
+		fd.append('name', createReadStream(__filename), { size: 0 });
+		expect(await readableToString(fd.stream)).toContain(`filename="${basename(__filename)}"`);
+	});
+});
+
+describe('FormData#length', () => {
+	it("should be correct when the Readable's size is (not) specified", () => {
+		const fd = new FormData();
+		fd.append('name', Readable.from('value'), { filename: 'abcd', size: 5 });
+		fd.append('name', Readable.from('value'), { filename: 'abcd', size: 0 });
+		// @ts-expect-error footer is private
+		expect(fd.length).toStrictEqual((110 + fd.boundary.length) * 2 + 5 + fd.footer.length);
+	});
+
+	it('should return the correct length', () => {
+		const fd = new FormData();
+		fd.append('name', Readable.from('value'), { filename: 'abcd', size: 5 });
+		fd.append('name', Buffer.from('value'), { filename: 'abcd' });
+		fd.append('name', 'value', { filename: 'abcd' });
+		fd.append('name', 'value');
+		fd.append('name', Buffer.from('value'));
+		// @ts-expect-error footer is private
+		expect(fd.length).toStrictEqual(115 * 3 + 58 * 2 + fd.boundary.length * 5 + fd.footer.length);
+	});
+});
+
+describe('FormData#stream', () => {
+	it('should return the correct body', async () => {
+		const fd = new FormData();
+		fd.append('name', 'value', { filename: 'abc' });
+		fd.append('name', Buffer.from('value'), { filename: 'abc' });
+		fd.append('name', Readable.from('value'), { filename: 'abc', size: 5 });
+		expect(await readableToString(fd.stream)).toStrictEqual(
+			`${DASHES}${
+				fd.boundary
+			}${CRLF}Content-Disposition: form-data; name="name"; filename="abc"${CRLF}Content-Type: application/octet-stream${CRLF.repeat(
+				2,
+				// @ts-expect-error footer is private
+			)}value${CRLF}`.repeat(3) + fd.footer,
+		);
+	});
+});
+
+describe('FormData#headers', () => {
+	it('should return the correct headers', () => {
+		const fd = new FormData();
+		fd.append('name', Readable.from('value'), { filename: 'abcd', size: 5 });
+		fd.append('name', Buffer.from('value'), { filename: 'abcd' });
+		fd.append('name', 'value', { filename: 'abcd' });
+		fd.append('name', 'value');
+		fd.append('name', Buffer.from('value'));
+		expect(fd.headers).toStrictEqual({
+			'Content-Type': `multipart/form-data; boundary=${fd.boundary}`,
+			'Content-Length': fd.length,
+		});
+	});
+
+	it('should not include content-length in the headers', () => {
+		const fd = new FormData(false);
+		fd.append('name', Readable.from('value'), { filename: 'abcd', size: 5 });
+		fd.append('name', Buffer.from('value'), { filename: 'abcd' });
+		fd.append('name', 'value', { filename: 'abcd' });
+		fd.append('name', 'value');
+		fd.append('name', Buffer.from('value'));
+		expect(fd.headers).toStrictEqual({
+			'Content-Type': `multipart/form-data; boundary=${fd.boundary}`,
+		});
+	});
+});
